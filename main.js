776054ad7ad32fd5e9fdada7549b758a2ce4b6cc
maxxxkam <muslimovmaxkam@gmail.com>
2024-07-30 11:05:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// for(let i = 20; i >= 10;  i--) {
+//    console.log(i);
+// }
+
+// let i = 0
+// while(i < 10) {
+//    console.log(i);
+//    i++
+// }
+
+
+// let i = 0
+// do {
+//    console.log(i);
+//    i++
+// }while (i > 5)
+
+// for (let i = 0; i < 10; i++) {
+//   if(i % 2 == 0) {
+//    console.log(i);
+   
+//   }
+// }
+
+
+// let sum = 0
+// for (let i = 0; i < 10; i++) {
+//    sum = sum + 1
+//  }
